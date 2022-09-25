966b0ed2f2a4d7fe301a377d5c923362d26e2fbb
BojanIstvancic <bojan.istvancic@gmail.com>
2022-09-25 16:55:06 +0200

    setup signIn component

--- src/components/signIn/SignIn.tsx
+++ src/components/signIn/SignIn.tsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 export interface SignInProps {}
 
 const SignIn: React.FC<SignInProps> = () => {
+  const [email, setEmail] = useState("emai@email.com");
+  const [password, setPassword] = useState("123");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
   return (
     <>
-      <p>SignIn component</p>
+      <form onSubmit={handleSubmit}>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="Sign Up" />
+      </form>
     </>
   );
 };
