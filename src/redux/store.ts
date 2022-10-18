d862c026b5d3a70254020dc5e97753ad30a3f416
BojanIstvancic <bojan.istvancic@gmail.com>
2022-10-18 14:51:40 +0200

    add redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import authenticationReducer from './authenticationSlice';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key:"root",
+  version: 1,
+  storage
+}
+
+const reducer = combineReducers({
+    authentication: authenticationReducer 
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
-  reducer: {
-    authentication: authenticationReducer
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 })
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
