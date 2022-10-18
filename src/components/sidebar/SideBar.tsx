cf30b5f5feca10bddda723903627474a186ef0ba
BojanIstvancic <bojan.istvancic@gmail.com>
2022-10-18 14:14:51 +0200

    add logo add settings to the bottom

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,9 +1,16 @@
-import { Drawer, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import AccessTimeFilledSharpIcon from "@mui/icons-material/AccessTimeFilledSharp";
 import { withRouter } from "react-router";
 import links from "../../helpers/links";
 import { RouteComponentProps } from "react-router-dom";
 import SettingsIcon from "@mui/icons-material/Settings";
+import logo from "../../assets/images/logo.png";
 
 interface SideBarProps extends RouteComponentProps {}
 
@@ -11,19 +18,24 @@ const SideBar: React.FunctionComponent<SideBarProps> = ({ history }) => {
   const currentRoute = history.location.pathname;
   return (
     <Drawer variant="permanent" anchor="left" sx={{ width: "200px" }}>
-      <ListItem
-        onClick={() => {
-          history.push(links.tracker.pattern);
-        }}
-        button
-        selected={currentRoute === links.tracker.pattern}
-        sx={{ width: "200px" }}
-      >
-        <ListItemIcon>
-          <AccessTimeFilledSharpIcon />
-        </ListItemIcon>
-        <ListItemText>Timer</ListItemText>
+      <ListItem sx={{ padding: "0" }}>
+        <img src={logo} alt="logo" style={{ width: "100%" }} />
       </ListItem>
+      <Box sx={{ height: "100%" }}>
+        <ListItem
+          onClick={() => {
+            history.push(links.tracker.pattern);
+          }}
+          button
+          selected={currentRoute === links.tracker.pattern}
+          sx={{ width: "200px" }}
+        >
+          <ListItemIcon>
+            <AccessTimeFilledSharpIcon />
+          </ListItemIcon>
+          <ListItemText>Timer</ListItemText>
+        </ListItem>
+      </Box>
       <ListItem
         onClick={() => {
           history.push(links.settings.pattern);
